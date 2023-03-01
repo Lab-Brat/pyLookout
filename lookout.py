11812adc3fd8c72a3128437cf0aa7154a582ace4
Lab-Brat <xoovoox@protonmail.com>
2023-03-01 23:54:26 +0300

    improve output, allow threshold input

--- lookout.py
+++ lookout.py
@@ -1,34 +1,33 @@
 from info_collector import Collector
 
 class PyLookout:
-    def __init__(self):
+    def __init__(self, threshold=80):
         self.info = Collector()
-        self.critical = 85
+        self.critical = threshold
 
-    def _stressed(self, percent):
+    def _stressed(self, metric, percent):
         '''
         Compare a metric with the critical value.
         '''
-        return True if percent > self.critical else False
+        stressed = True if percent > self.critical else False
+
+        if stressed:
+            self._notify(metric, percent)
     
-    def _notify(self, metric):
+    def _notify(self, metric, percent):
         '''
         Send a notification.
         (right now it's just a print statement).
         '''
-        print(f'Danger! ---> {metric} > {self.critical}%')
+        print(f'Danger! ---> {metric} = {percent}')
 
     def checker(self):
         '''
         One by one check if CPU, RAM and Disk space 
         utilization is larger than the critical value.
         '''
-        if self._stressed(self.info.cpu_percent):
-            self._notify('CPU')
-        
-        if self._stressed(self.info.ram_percent):
-            self._notify('RAM')
+        self._stressed('CPU', self.info.cpu_percent)
+        self._stressed('RAM', self.info.ram_percent)
 
         for disk in self.info.disks_info.values():
-            if self._stressed(disk['du_percent']):
-                self._notify('DISK')
+            self._stressed('DISK', disk['du_percent'])
