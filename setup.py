cede3c51b7941e996c3726bf0444e0b947f70faa
Lab-Brat <xoovoox@protonmail.com>
2023-03-05 00:35:31 +0300

    update setup.py
    temporary workaround to use 'scripts', which places all files
    to PATH directory. This pollutes the directory, and requires
    proper packaging.

--- setup.py
+++ setup.py
@@ -1,12 +1,37 @@
-from setuptools import setup
+import pathlib
+from setuptools import setup, find_packages
+
+here = pathlib.Path(__file__).parent.resolve()
+
+with open("./README.md", "r") as rm:
+    long_description = rm.read()
 
 setup(
     name="pylookout",
     version="0.1.0",
-    py_modules=[],
+    description="Simple Linux system monitoring tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Lab-Brat/pyLookout",
+    author="Lab-Brat",
+    author_email="labbrat_social@pm.me",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: Linux",
+    ],
+    packages=find_packages(),
+    scripts=[
+        "main.py",
+        "info_collector.py",
+        "lookout.py",
+    ],
     install_requires=[
         "psutil",
+        "sendgrid",
     ],
+    python_requires=">=3.6",
     entry_points={
         "console_scripts": [
             "pylookout = main:main",
